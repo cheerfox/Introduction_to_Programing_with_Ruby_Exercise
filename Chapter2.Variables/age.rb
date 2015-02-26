802c612aa0c2162bf5df11862f184f66847b3b15
Wang Chun Chi <limitsea0103@gmail.com>
2015-02-26 22:25:50 +0800

    Finish Chapter2 exercises

---
+++ Chapter2.Variables/age.rb
@@ -0,0 +1,6 @@
+puts "How old are you??"
+age = gets.chomp.to_i
+puts "In 10 year you will be #{age+10}"
+puts "In 20 year you will be #{age+20}"
+puts "In 30 year you will be #{age+30}"
+puts "In 40 year you will be #{age+40}"
---
+++ Chapter2.Variables/name.rb
@@ -0,0 +1,8 @@
+puts "Input your fisrt name"
+first_name = gets.chomp
+puts "Input your last name"
+last_name = gets.chomp
+puts "Hello #{first_name + last_name}!!"
+10.times do
+  puts first_name + last_name
+end
